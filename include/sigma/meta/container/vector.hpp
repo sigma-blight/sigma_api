ce1a23a045cdd95f550b8b0ea7993b1355d57160
Fletcher Blight <fletcher.register@gmail.com>
2017-09-04 19:14:40 +1000

    created value vector

--- include/sigma/meta/container/vector.hpp
+++ include/sigma/meta/container/vector.hpp
@@ -80,6 +80,93 @@ namespace sigma::meta
 		constexpr bool operator == (TVector<Others_...>) const { return false; }
 		constexpr bool operator == (TVector<Types_...>) const { return true; }
 	};
+
+
+
+	template <auto... values_>
+	class VVector
+	{
+		template <std::size_t index_, auto first_, auto... trail_>
+		static constexpr auto get_impl(void)
+		{
+			if constexpr (index_ == 0) return first_;
+			else return get_impl<index_ - 1, trail_...>();
+		}
+
+	public:
+
+		static constexpr std::size_t size(void) { return sizeof...(values_); }
+
+		template <std::size_t index_>
+		static constexpr auto get(std::integral_constant<std::size_t, index_>)
+		{
+			static_assert(index_ < size(), "index is out of bounds");
+			return get_impl<index_, values_...>();
+		}
+
+		template <std::size_t index_>
+		static constexpr auto get(void) { return get(std::integral_constant<std::size_t, index_>{}); }
+
+		template <std::size_t index_>
+		constexpr auto operator[] (std::integral_constant<std::size_t, index_> index) const
+		{
+			return get(index);
+		}
+
+		static constexpr auto front(void) { return get<0>(); }
+		static constexpr auto back(void) { return get<size() - 1>(); }
+
+	private:
+
+		static constexpr void assert_safe_pop(void)
+		{
+			static_assert(size() != 0, "cannot pop from no elements");
+		}
+
+		template <auto, auto... trail_>
+		static constexpr auto pop_front_impl(void) { return VVector<trail_...>{}; }
+
+		template <typename Vector_, auto first_, auto... trail_>
+		static constexpr auto pop_back_impl(void)
+		{
+			if constexpr (VVector<trail_...>::size() == 0) return Vector_{};
+			else
+			{
+				using vvector_t = decltype(Vector_::push_back(first_));
+				return pop_back_impl<vvector_t, trail_...>();
+			}
+		}
+
+	public:
+
+		template <auto value_>
+		static constexpr auto push_back(std::integral_constant<decltype(value_), value_>)
+		{
+			return VVector<values_..., value_>{};
+		}
+
+		template <auto value_>
+		static constexpr auto push_front(std::integral_constant<decltype(value_), value_>)
+		{
+			return VVector<value_, values_...>{};
+		}
+
+		static constexpr auto pop_front(void)
+		{
+			assert_safe_pop();
+			return pop_front_impl<values_...>();
+		}
+
+		static constexpr auto pop_back(void)
+		{
+			assert_safe_pop();
+			return pop_back_impl<values_...>();
+		}
+
+		template <auto... others_>
+		constexpr bool operator == (VVector<others_...>) const { return false; }
+		constexpr bool operator == (VVector<values_...>) const { return true; }
+	};
 }
 
 #endif	//	_SIGMA_API_META_CONTINER_VECTOR_HPP_
